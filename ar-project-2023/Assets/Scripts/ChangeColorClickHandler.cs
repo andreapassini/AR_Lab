3c39656a9d39f1fd94ae9a322d901e0b54ee427c
Andrea Passini <andreapa98@gmail.com>
2023-04-22 21:32:18 +0200

    final apk build

--- ar-project-2023/Assets/Scripts/ChangeColorClickHandler.cs
+++ ar-project-2023/Assets/Scripts/ChangeColorClickHandler.cs
@@ -1,21 +1,41 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using DefaultNamespace;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
+[RequireComponent(typeof(Animator))]
 public class ChangeColorClickHandler : MonoBehaviour, IPointerClickHandler
 {
-    private Renderer _renderer;
-    
-    // Start is called before the first frame update
-    void Start()
+    public AudioClip audioClip;
+    private Animator _animator;
+    private static readonly int _Touch = Animator.StringToHash("touch");
+    private bool _playingAnim = false;
+
+    private void Awake()
     {
-        _renderer = GetComponent<Renderer>();
+        _animator ??= transform.GetComponent<Animator>();
     }
 
     public void OnPointerClick(PointerEventData pointerEventData)
     {
-        _renderer.material.color =
-            _renderer.material.color == Color.yellow ? Color.green : Color.yellow;
+        if(_playingAnim)
+            return;
+        
+        _playingAnim = true;
+        // Start Animation
+        _animator.SetTrigger(_Touch);
+    }
+
+    public void PlayShootingSfx()
+    {
+        // Play sound
+        AudioManager.Instance.PlayClip(audioClip);
+    }
+
+    public void EndAnimation()
+    {
+        _playingAnim = false;
     }
 }
