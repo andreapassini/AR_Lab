16ae6b4e60880de85bfd9bf503f2859a580a9c25
Andrea Passini <andreapa98@gmail.com>
2023-05-02 20:41:48 +0200

    Lab test

---
+++ ar-project-2023/Assets/Scripts/DisableOnAwake.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableOnAwake : MonoBehaviour
+{
+    private void Awake()
+    {
+        gameObject.SetActive(false);
+    }
+}
